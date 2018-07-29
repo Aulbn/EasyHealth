b0862e7e0af97d33dd1b23de0b3032eaa61d1ec2
Albin <38533964+Aulbn@users.noreply.github.com>
2018-07-29 14:22:27 +0200

    Vet inte
    Vet inte riktigt vad som hände här. Det blev något slags error och sen en massa merges, typ.

---
+++ EasyHealth/Assets/Scripts/StatsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsManager : MonoBehaviour {
+	public static StatsManager instance;
+
+	public GraphDisplay graphDisplay;
+	public DropdownScript dropDown;
+
+	void Awake(){
+		instance = this;
+	}
+
+	public void LoadExerciseList(){
+		dropDown.SetList (WorkoutManager.instance.LoadExerciseList ());
+	}
+
+}
